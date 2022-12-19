829a7b1d2fa34a15c8bccd879333f5f0fb0c2138
RiceGrain1 <102405076+RiceGrain1@users.noreply.github.com>
2022-12-19 00:30:14 -0600

    Fixed naming issue

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ const massOutput = document.getElementById("converted-mass")
 
 
 function convert(unitToUnit1,unitToUnit2,elementID, conversionFactor) {
-    unitToUnit1 = Number(conversionValue.value) * conversionFactor
-     unitToUnit2 = Number(conversionValue.value) / conversionFactor
+    let convertExport1 = Number(conversionValue.value) * conversionFactor
+     let convertExport2 = Number(conversionValue.value) / conversionFactor
      elementID.innerHTML = `
-    <p id="converted-length" class="converted-output-txt">${conversionValue.value} meters = ${unitToUnit1.toFixed(3)} feet | ${conversionValue.value} feet = ${unitToUnit2.toFixed(3)} meters</p>`
+    <p id="converted-length" class="converted-output-txt">${conversionValue.value} ${unitToUnit1} = ${convertExport1.toFixed(3)} ${unitToUnit2} | ${conversionValue.value} ${unitToUnit2} = ${convertExport2.toFixed(3)} ${unitToUnit1}</p>`
 }
 
 convertBtn.addEventListener("click", () => {
-    convert("metersToFeet", "feetToMeters", lengthOutput, 3.281)
-    convert("litersToGallons", "gallonsToLiters", volumeOutput, 0.264)
-    convert("kilogramsToPounds", "poundsToKilograms", massOutput, 2.204)
+    convert("meters", "feet", lengthOutput, 3.281)
+    convert("liters", "gallons", volumeOutput, 0.264)
+    convert("kilograms", "pounds", massOutput, 2.204)
 })
+
